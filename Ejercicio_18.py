badda123f4ce13115f7b2f9a2e7a1342440d8948
FajardoSF <102683807+FajardoSF@users.noreply.github.com>
2022-04-10 19:32:02 -0400

    Delete Ejercicio_18.py

--- Ejercicio_18.py
+++
@@ -1,9 +0,0 @@
-# Entradas
-x = float(input("¿De cuantos bolivares fue el prestamo?"))
-i = float(input("¿Cuánto se pago por intereses?"))
-
-# Caja negra
-a = ((i*100)/x)/4
-
-# Salida 
-print(f"El interes anual fue de {a}%")
