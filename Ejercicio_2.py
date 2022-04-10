e109df086a5e5603eebeee56235276e7095e1ee5
FajardoSF <102683807+FajardoSF@users.noreply.github.com>
2022-04-10 19:32:38 -0400

    Delete Ejercicio_2.py

--- Ejercicio_2.py
+++
@@ -1,9 +0,0 @@
-# Entradas
-a = float(input("Dime el dinero que vas a invertir\n"))
-
-# Caja negra
-b = a*0.02
-c = b + a 
-
-# Salidas
-print(f"Tu capital total en un mes es {c} es decir ganaste {b} ") 
