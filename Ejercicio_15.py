7daa197ef374db6c45049f684c458dea677221d6
FajardoSF <102683807+FajardoSF@users.noreply.github.com>
2022-04-10 19:30:49 -0400

    Delete Ejercicio_15.py

--- Ejercicio_15.py
+++
@@ -1,9 +0,0 @@
-# Entradas
-PVP = float(input("\nDime cual es el precio de venta al público "))
-Pf = float(input("Dime cual es el precio final "))        
-
-# Caja negra 
-D = 100 -((Pf*100)/PVP)
-
-# Salida
-print(f"\nTu descuento fue de {D}%\n")
